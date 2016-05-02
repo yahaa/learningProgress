7de7b98b1f12879d0389f765e6c668c0d3cd956d
yahaa <1477765176@qq.com>
2016-05-02 09:25:23 +0800

    sss

---
+++ Just_keep_going.py
@@ -0,0 +1,39 @@
+1. Make sacrifices
+2. One move at a time
+3. Trust your skills
+4. Use your scars as a weapon
+5. Focus on each day
+6. Don't be afraid of confrontation
+7. Be competitive when it's hard
+8. Just keep going
+9. Thrive on being an outsider
+10. Compete with yourself
+
+BY Kobe
+
+
+1. Get up, do it again
+2. Know what you want 
+3. Find what you're good at 
+4. #Believe in yourself
+5. Learn from others
+6. Don't be afraid to try
+7. Take the good with the bad
+8. Find your inspiration
+9. Be determined
+10. It's all love
+
+BY Eminem's
+
+1. Get used to rejection
+2. Keep your dream alive 
+3. Focus on culture
+4. Ignore the #LittleMan
+5. Get inspired
+6. Stay focused
+7. Have a good name 
+8. Customers are #1 
+9. Don't complain, look for opportunities
+10. Have passion
+
+BU Jack Ma's
